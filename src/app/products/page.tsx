edcf3ee4242e45f3ee5cf5234c5d98db40964dff
LucasLimaDSGN <lucaslima.cont@gmail.com>
2025-02-21 23:53:46 -0300

    feat: add shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <Button>Comprar</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
